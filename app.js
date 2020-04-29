3f84536815ef29307efc0e0f3f9e6c855f9d8453
Buzka14 <stoycheva.liza@gmail.com>
2020-04-29 14:55:16 +0100

    set up memorygame project

---
+++ app.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cardArray = [
+        {
+            name: "ajax",
+            img: 'image/ajax'
+        },
+        {
+            name: "ajax",
+            img: 'image/ajax'
+        },
+        {
+            name: "bdortmund",
+            img: 'image/bdortmund'
+        },
+        {
+            name: "bdortmund",
+            img: 'image/bdortmund'
+        },
+        {
+            name: "inter",
+            img: 'image/inter'
+        },
+        {
+            name: "inter",
+            img: 'image/inter'
+        },
+        {
+            name: "lpool",
+            img: 'image/lpool'
+        },
+        {
+            name: "lpool",
+            img: 'image/lpool'
+        },
+        {
+            name: "manutd",
+            img: 'image/manutd'
+        },
+        {
+            name: "manutd",
+            img: 'image/manutd'
+        },
+        {
+            name: "rmadrid",
+            img: 'image/rmadrid'
+        },{
+            name: "rmadrid",
+            img: 'image/rmadrid'
+        }
+    ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
